ef3284c899c73a919e2eb6b5d252f66bbf0fd63a
namjoker <nameffect@gmail.com>
2016-08-12 11:23:37 +0700

    sua doi loi html mail

--- vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
+++ vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
@@ -1,9 +1,9 @@
-Hey {{ $user->username }}!
+Chào {{ $user->username }}!
 
-{{ $blueprint->post->user->username }} mentioned you in a post in {{ $blueprint->post->discussion->title }}.
+{{ $blueprint->post->user->username }} được nhắc tới trong bài viết {{ $blueprint->post->discussion->title }}.
 
 {{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ $blueprint->post->number }}
 
 ---
 
-{{ strip_tags($blueprint->post->contentHtml) }}
+{{ strip_tags($blueprint->post->contentHtml }}
