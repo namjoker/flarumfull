8e5683c80d32e235fe38083e82497e5c4edbc2af
namjoker <nameffect@gmail.com>
2016-08-12 11:31:39 +0700

    utf8_encode

--- vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
+++ vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
@@ -2,7 +2,7 @@ Chào {{ $user->username }}!
 
 {{ $blueprint->post->user->username }} được nhắc tới trong bài viết {{ $blueprint->post->discussion->title }}.
 
-{{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ $blueprint->post->number }}
+{{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ utf8_encode($blueprint->post->number) }}
 
 ---
 
