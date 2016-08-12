65a0c6e180899c9cba90f096f11659bae6e7d6e9
namjoker <nameffect@gmail.com>
2016-08-12 11:38:27 +0700

    html_entities

--- vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
+++ vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
@@ -1,8 +1,8 @@
 Chào {{ $user->username }}!
 
-{{ $blueprint->post->user->username }} được nhắc tới trong bài viết {{ $blueprint->post->discussion->title }}.
+{{ $blueprint->post->user->username }} được nhắc tới trong bài viết {{ html_entities($blueprint->post->discussion->title) }}.
 
-{{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ utf8_encode($blueprint->post->number) }}
+{{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ html_entities($blueprint->post->number) }}
 
 ---
 
