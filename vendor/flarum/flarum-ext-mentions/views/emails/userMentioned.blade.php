40df79496aa7fb921967a053f4fae275109d569d
namjoker <nameffect@gmail.com>
2016-08-12 13:48:59 +0700

    update thêm nội dung

--- vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
+++ vendor/flarum/flarum-ext-mentions/views/emails/userMentioned.blade.php
@@ -3,4 +3,6 @@
 -{{ $blueprint->post->user->username }} đã nhắc tới bạn trong bài viết
  
  {{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ $blueprint->post->number }}
-<h1>Bấm vào link để xem bào viết</h1> 
+
+Bấm vào liên kết để xem nội dung bài viết.
+
