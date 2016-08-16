b4777baeb15873854cfd8451f49bfe31761c6eca
namjoker <nameffect@gmail.com>
2016-08-16 08:30:55 +0700

    update slug

--- vendor/flarum/core/js/lib/utils/string.js
+++ vendor/flarum/core/js/lib/utils/string.js
@@ -21,8 +21,14 @@ export function truncate(string, length, start = 0) {
  */
 export function slug(string) {
   return string.toLowerCase()
-    .replace(/[^a-z0-9]/gi, '-')
-    .replace(/-+/g, '-')
+    .replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a')
+    .replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e')
+    .replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i')
+    .replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o')
+    .replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u')
+    .replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y')
+    .replace(/đ/gi, 'd')
+    .replace(/-$|^-/g, '') || '-';
     .replace(/-$|^-/g, '') || '-';
 }
 
